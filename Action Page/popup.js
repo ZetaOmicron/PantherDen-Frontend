a1664805f15e42c7dd23c500316385609e5a4e15
Ryan Wells <rawells14@gmail.com>
2014-11-04 11:29:11 -0600

    Change the Action Page

--- Action Page/popup.js
+++ Action Page/popup.js
@@ -1,6 +1,13 @@
 $(document).ready(function () {
-    $('#welcome').hide();
-    $('#actionbutton').click(function () {
-        $('#welcome').show();
+    $("#welcome").hide();
+
+    
+    $("#actionbutton").click(function () {
+
+        $("#welcome").show();
+    });
+
+    $("#actionDropDown").click(function () {
+        $(this).dropdown();
     });
 });
