4f2c6a86feedd07c1bd8b6e90519122a13a65b72
Ryan Wells <rawells14@gmail.com>
2014-11-05 11:42:53 -0600

    Fixed Action Button/PopUp

--- Action Page/popup.js
+++ Action Page/popup.js
@@ -1,9 +1,18 @@
 $(document).ready(function () {
     $("#actionLabelWrapper").hide();
-
-    $("#actionbutton").click(function () {
+    $("#cover").hide();
+    var flag = 0;
+    $("#buttonwrapper").click(function () {
+        flag = 1;
         $("#actionLabelWrapper").show();
+        $("#cover").show();
     });
+
+    $("#cover").click(function () {
+        $("#actionLabelWrapper").hide();
+        $("#cover").hide();
+    });
+
 
     $("#actionDropDown").click(function () {
         $(this).dropdown();
