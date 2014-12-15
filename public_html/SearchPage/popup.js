09801306785665ff223ffc2ea2163d534e4dde42
Michael Remediakis <m_remedi@yahoo.com>
2014-12-15 11:13:23 -0600

    Redesigned the Calendar Search Bar

--- public_html/SearchPage/popup.js
+++ public_html/SearchPage/popup.js
@@ -1,15 +1,18 @@
 $(document).ready(function () {
     $("#actionLabelWrapper").hide();
+    $("#infowrapper").hide();
     $("#cover").hide();
     var flag = 0;
     $("#buttonwrapper").click(function () {
         flag = 1;
-        $("#actionLabelWrapper").fadeIn(300);
+       // $("#actionLabelWrapper").fadeIn(300);
+        $("#infowrapper").fadeIn(300);
         $("#cover").fadeIn(300);
     });
 
     $("#cover").click(function () {
-        $("#actionLabelWrapper").fadeOut(300);
+     //  $("#actionLabelWrapper").fadeOut(300);
+       $("#infowrapper").fadeOut(300);
         $("#cover").fadeOut(300);
     });
 
