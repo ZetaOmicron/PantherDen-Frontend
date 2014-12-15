82d16c52f2c5823cf8d71b1b47563d479aaa70df
Michael Remediakis <m_remedi@yahoo.com>
2014-12-15 16:25:17 -0600

    Did some stuff
    ...

--- public_html/SearchPage/popup.js
+++ public_html/SearchPage/popup.js
@@ -5,14 +5,14 @@ $(document).ready(function () {
     var flag = 0;
     $("#buttonwrapper").click(function () {
         flag = 1;
-       // $("#actionLabelWrapper").fadeIn(300);
-        $("#infowrapper").fadeIn(300);
+        $("#actionLabelWrapper").fadeIn(300);
+       // $("#infowrapper").fadeIn(300);
         $("#cover").fadeIn(300);
     });
 
     $("#cover").click(function () {
-     //  $("#actionLabelWrapper").fadeOut(300);
-       $("#infowrapper").fadeOut(300);
+       $("#actionLabelWrapper").fadeOut(300);
+     //  $("#infowrapper").fadeOut(300);
         $("#cover").fadeOut(300);
     });
 
