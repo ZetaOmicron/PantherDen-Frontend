54e70fe0e7ab348889b09e8ac2cd8b94a044931b
5pitty1 <m_remedi@yahoo.com>
2015-01-05 11:12:03 -0600

    Started the queue box

--- public_html/Teachers/SearchPage/SearchScript.js
+++ public_html/Teachers/SearchPage/SearchScript.js
@@ -1,16 +1,5 @@
 $(document).ready(function() {
-
+    $('#mainbox').hover(function () {
+        $('#queuebox').addClass("enter");
+    });
 });
-
-
-function searchtype(input) {
-    if(input.charAt(0)=='4') {
-        return "ID";
-    }
-    if(input.charAt(0)=='1'||input.charAt(0)=='2') {
-        return "Room";
-    }
-    if(input.substring(0,2).toUpperCase()=="AUD") {
-        return "Room";
-    }
-};
