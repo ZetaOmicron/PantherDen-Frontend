cdedbd61477527877e4db579c9488afe74764d9c
Ryan Wells <rawells14@gmail.com>
2015-01-06 14:26:24 -0600

    Fixed Data manager Page navigation
    In the beginning the data managers couldn't navigate between pages. Now,
    data managers can go between each page (conflict and search as of now).
    Also, the file structure in the data manager's folder has been improved
    and will be easier to implement new pages.

---
+++ public_html/DataManagers/SearchPage/SearchScript.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('#mainbox').hover(function () {
+        $('#queuebox').addClass("enter");
+    });
+});
--- public_html/DataManagers/SearchPage/popup.js
+++ public_html/DataManagers/SearchPage/popup.js
@@ -1,16 +1,19 @@
 $(document).ready(function () {
     $("#actionLabelWrapper").hide();
+    $("#infowrapper").hide();
     $("#cover").hide();
-    var flag = 0; 
+    var flag = 0;
     $("#buttonwrapper").click(function () {
         flag = 1;
-        $("#actionLabelWrapper").show();
-        $("#cover").show();
+        $("#actionLabelWrapper").fadeIn(300);
+       // $("#infowrapper").fadeIn(300);
+        $("#cover").fadeIn(300);
     });
 
     $("#cover").click(function () {
-        $("#actionLabelWrapper").hide();
-        $("#cover").hide();
+       $("#actionLabelWrapper").fadeOut(300);
+     //  $("#infowrapper").fadeOut(300);
+        $("#cover").fadeOut(300);
     });
 
 
