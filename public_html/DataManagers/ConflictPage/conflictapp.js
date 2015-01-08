eb2e5ca8667dc2b1b645b27baaf5aad580286db2
Ryan Wells <rawells14@gmail.com>
2015-01-08 13:57:31 -0600

    Completed Add conflict method
    They should tile correctly now

--- public_html/DataManagers/ConflictPage/conflictapp.js
+++ public_html/DataManagers/ConflictPage/conflictapp.js
@@ -1,6 +1,6 @@
 var amtOfConflicts = 0;
 $(document).ready(function () {
-    
+
     $("#accept-submission1").click(function () {
         $("#content-row1").hide();
     });
@@ -10,13 +10,18 @@ $(document).ready(function () {
 });
 
 //adds a new conflict bar to the page
-function addConflict(student, startingClass, classToTransfer, date, conflictID) {
-    var toAppend = '<tr id="content-row' + conflictID + '"><td>' + student + '</td><td>' + startingClass + ' &#8594; ' + classToTransfer + '</td><td>' + date + '</td><td><div class="accept-decline-pair"><a href="#"><span  id= "accept-submission' + conflictID + '" class="glyphicon glyphicon-ok-circle accept" aria-hidden="true"></span></a> &nbsp;<a href="#"> <span id="deny-submission' + conflictID + '" class="glyphicon glyphicon-remove-circle decline" aria-hidden="true"></span></a></div></td></tr>';
+function addConflict(student, startingClass, classesToTransfer, date, conflictID) {
+    var toAppend = '<tr id="content-row' + conflictID + '"><td>' + student + '</td><td>' + startingClass + '&#8594;';
+    for (var i = 0; i < classesToTransfer.length; i++) {
+        toAppend += '<button type="button" class="btn btn-default">' + classesToTransfer[i] + '</button>';
+    }
+
+    toAppend = toAppend + '</td><td>' + date + '</td><td><div class="accept-decline-pair"><a href="#"><span  id= "accept-submission' + conflictID + '" class="glyphicon glyphicon-ok-circle accept" aria-hidden="true"></span></a></div></td></tr>';
     $("#conflict-table-body").append($(toAppend));
-    $("#accept-submission" + conflictID+"").click(function () {
+
+
+    $("#accept-submission" + conflictID + "").click(function () {
         $("#content-row" + conflictID + "").hide();
     });
-    $("#deny-submission" + conflictID + "").click(function () {
-        $("#content-row" + conflictID + "").hide();
-    });
+
 }
